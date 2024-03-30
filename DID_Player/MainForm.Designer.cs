4d790fae0890456222fb16f4b25ab849f4b722b0
BaeTab <bhwoo48@gmail.com>
2024-03-30 20:57:14 +0900

    - init Commit

---
+++ DID_Player/MainForm.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace DID_Player
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(346, 536);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "사진 목록";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(265, 78);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "초기화";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button6.Location = new System.Drawing.Point(265, 493);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "불러오기";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button5.Location = new System.Drawing.Point(265, 464);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "저장";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "제외";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "추가";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(6, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 496);
+            this.listBox1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(376, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(286, 536);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "설정";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "사진 전환 순서";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "사진 전환 간격(초)";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "이미지 미리보기";
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(205, 507);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "종료";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(274, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(123, 507);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "시작";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(160, 243);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "순차적",
+            "무작위"});
+            this.comboBox1.Location = new System.Drawing.Point(159, 284);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(6, 519);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 560);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(690, 599);
+            this.Name = "MainForm";
+            this.Text = "DID Player";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DID_Player/MainForm.cs
@@ -0,0 +1,338 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DID_Player
+{
+    public partial class MainForm : Form
+    {
+        private Form fullScreen = null;
+        private PictureBox pictureBox = null;
+        private int currentIndex = 0;
+
+        Dictionary<string, Image> images = new Dictionary<string, Image>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            initEvent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void initEvent()
+        {
+            button1.Click += Button1_Click;   // 파일 추가
+            button2.Click += Button2_Click;   // 선택 삭제
+            button3.Click += Button3_Click;   // 재생
+            button4.Click += Button4_Click;   // 종료
+            button5.Click += Button5_Click;   // 저장
+            button6.Click += Button6_Click;   // 불러오기
+            button7.Click += Button7_Click;   // 초기화
+
+            listBox1.DragEnter += listBox1_DragEnter;
+            listBox1.DragDrop += listBox1_DragDrop;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                showImage();
+            }
+            else
+            {
+                Random random = new Random();
+                currentIndex = random.Next(0, listBox1.Items.Count);
+                showImage();
+            }
+        }
+
+
+        // 리스트박스 인덱스가 바뀔때 미리보기를 할 수 있다.
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string imagePath = listBox1.SelectedItem.ToString();
+                pictureBox1.Image = images[imagePath];
+            }
+            else
+            {
+                pictureBox1.Image = null;
+            }
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                foreach (string file in files)
+                {
+                    string extension = Path.GetExtension(file).ToLower();
+                    if (extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".bmp")
+                    {
+                        e.Effect = DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.Effect = DragDropEffects.None; // 이미지 파일이 아닌 경우 드롭 금지
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                addImage(files);
+            }
+        }
+
+        // 추가
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Image Files | *.jpg;*png;*.jpeg;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                addImage(openFileDialog.FileNames);
+            }
+        }
+
+        // 리스트 제외
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != 1)
+            {
+                string itemPath = listBox1.SelectedItem.ToString();
+                listBox1.Items.Remove(itemPath);
+                images.Remove(itemPath);
+            }
+            string totalCount = listBox1.Items.Count.ToString();
+            label4.Text = $"Total : {totalCount}";
+        }
+
+        // 슬라이드쇼 시작
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            currentIndex = 0;
+            if (listBox1.Items.Count == 0) return;
+            if (fullScreen == null && pictureBox == null)
+            {
+                setFullScreen();
+            }
+            else
+            {
+                fullScreen = null;
+                pictureBox = null;
+                setFullScreen();
+            }
+        }
+
+        // 종료
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // 저장
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON 파일|*.json";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string json = JsonConvert.SerializeObject(images.Keys.ToList());
+                    File.WriteAllText(saveFileDialog.FileName, json);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"오류: {ex.Message}");
+                }
+            }
+        }
+
+        // 불러오기
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON 파일|*.json";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (listBox1.Items.Count != 0)
+                {
+                    listBox1.Items.Clear();
+                    images.Clear();
+                }
+                try
+                {
+                    string json = File.ReadAllText(openFileDialog.FileName);
+                    List<string> files = JsonConvert.DeserializeObject<List<string>>(json);
+
+                    addImage(files.ToArray());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"오류: {ex.Message}");
+                }
+            }
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count != 0)
+            {
+                listBox1.Items.Clear();
+                images.Clear();
+                pictureBox1.Image = null;
+            }
+            string totalCount = listBox1.Items.Count.ToString();
+            label4.Text = $"Total : {totalCount}";
+        }
+
+        // 추가된 이미지를 리스트박스와 딕셔너리에 추가하는 메서드
+        private void addImage(string[] files)
+        {
+            // 파일 배열이 null이 아니고 최소한 하나의 파일을 포함하고 있는지 확인 (length 로 확인)
+            if (files != null && files.Length > 0)
+            {
+                foreach (string file in files)
+                {
+                    // 파일이 이미 딕셔너리에 없는지 확인
+                    if (!images.ContainsKey(file))
+                    {
+                        try
+                        {
+                            Image img = Image.FromFile(file);           // 파일로부터 이미지를 로드
+                            images.Add(file, img);                           // 이미지를 파일 경로를 키로하여 딕셔너리에 추가
+                            listBox1.Items.Add(file);                         // 파일 경로를 리스트 박스에 추가
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"오류: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                listBox1.SelectedIndex = 0;         // 이미지 추가후 미리보기를 위해 0번 인덱스 셀렉트
+                string totalCount = listBox1.Items.Count.ToString();
+                label4.Text = $"Total : {totalCount}";
+            }
+        }
+
+        // ESC 키 슬라이드쇼 종료 이벤트
+        private void FullScreen_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                fullScreenClose();
+            }
+        }
+
+        // 좌클릭시 슬라이드쇼 종료 이벤트
+        private void PictureBox_LeftClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                fullScreenClose();
+            }
+        }
+
+        // 풀스크린 실행 메서드
+        private void setFullScreen()
+        {
+            fullScreen = new Form
+            {
+                FormBorderStyle = FormBorderStyle.None,
+#if DEBUG
+                WindowState = FormWindowState.Normal,
+#else
+                WindowState = FormWindowState.Maximized,
+#endif                
+                TopMost = true
+            };
+
+            pictureBox = new PictureBox
+            {
+                Dock = DockStyle.Fill,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = images[listBox1.Items[currentIndex].ToString()]
+            };
+            currentIndex++;
+
+            fullScreen.Controls.Add(pictureBox);
+            fullScreen.KeyPreview = true;
+            fullScreen.Show();
+            Cursor.Hide();
+
+            fullScreen.KeyDown += FullScreen_KeyDown;
+            pictureBox.MouseClick += PictureBox_LeftClick;
+            //pictureBox.MouseClick += PictureBox_RightClick;
+
+            timer1.Interval = (int)numericUpDown1.Value * 1000;
+            timer1.Start();
+        }
+
+
+        // 인덱스에 따른 이미지 표시를 위한 메서드
+        private void showImage()
+        {
+            if (currentIndex >= listBox1.Items.Count)
+            {
+                currentIndex = 0;
+            }
+            string imagePath = listBox1.Items[currentIndex].ToString();
+
+            pictureBox.Image = images[imagePath];
+            currentIndex++;
+        }
+
+        // 풀스크린 종료 메서드
+        private void fullScreenClose()
+        {
+            timer1.Stop();
+            Cursor.Show();
+            if (DialogResult.Yes == MessageBox.Show("종료 하시겠습니까?", "알림", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                if (currentIndex != 0)
+                {
+                    currentIndex = 0;
+                }
+                if (fullScreen != null)
+                {
+                    fullScreen.Dispose();
+                    fullScreen = null;
+                }
+                if (pictureBox != null)
+                {
+                    pictureBox.Dispose();
+                    pictureBox = null;
+                }
+                Show();
+            }
+            else
+            {
+                Cursor.Hide();
+                timer1.Start();
+            }
+        }
+    }
+}
